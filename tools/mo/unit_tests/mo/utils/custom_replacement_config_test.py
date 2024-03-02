55499d2139ca1e52c85f7a61ae4a63c861e365f1
taho7766 <wjtw9802@icloud.com>
2024-03-01 18:18:03 -0700

    updated custom_replacement_config_test.py

--- tools/mo/unit_tests/mo/utils/custom_replacement_config_test.py
+++ tools/mo/unit_tests/mo/utils/custom_replacement_config_test.py
@@ -10,7 +10,9 @@ from openvino.tools.mo.utils.error import Error
 from openvino.tools.mo.utils.utils import get_mo_root_dir
 from unit_tests.mo.unit_test_with_mocked_telemetry import UnitTestWithMockedTelemetry
 
-
+"""
+get_json_config may not work correctly
+"""
 def get_json_configs(mo_root_dir):
     config_path = os.path.join(mo_root_dir, 'extensions', 'front')
     pattern = "*.json"
@@ -21,7 +23,6 @@ def get_json_configs(mo_root_dir):
                 config_files_list.append((os.path.join(path, name),))
     return config_files_list
 
-@generator
 class TestSchema(UnitTestWithMockedTelemetry):
     base_dir = get_mo_root_dir()
     schema_file = os.path.join(base_dir, 'mo', 'utils', 'schema.json')
@@ -29,6 +30,7 @@ class TestSchema(UnitTestWithMockedTelemetry):
     test_json1 = '[{"id": "", "match_kind": "general", "custom_attributes": {}}]'
     test_json2 = '[{"id": "someid", "match_kind": "abc", "custom_attributes": {}}]'
 
+    """
     @generate(*transformation_configs)
     def test_schema_file(self, transformation_config):
         self.assertTrue(load_and_validate_json_config(transformation_config))
@@ -38,3 +40,19 @@ class TestSchema(UnitTestWithMockedTelemetry):
 
     def test_schema_match_kind_wrong(self):
         self.assertRaises(Error, load_and_validate_json_config, self.test_json2)
+    """
+    
+    
+    def test_schema_files(self):
+        for transformation_config in self.transformation_configs:
+            with self.subTest(transformation_config=transformation_config):
+                self.assertTrue(load_and_validate_json_config(transformation_config))
+
+    def test_schema_id_empty(self):
+        with self.assertRaises(Error):
+            load_and_validate_json_config(self.test_json1)
+
+    def test_schema_match_kind_wrong(self):
+        with self.assertRaises(Error):
+            load_and_validate_json_config(self.test_json2)
+
